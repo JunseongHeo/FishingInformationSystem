4a83b909dc07d6ade5a5c2ec487c79a5fc8b9a80
qldhsek1004 <106101558+qldhsek1004@users.noreply.github.com>
2022-06-09 14:29:25 +0900

    6/9일

--- member/src/member/MessageBoard.java
+++ member/src/member/MessageBoard.java
@@ -1,11 +1,13 @@
 package member;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MessageBoard extends LoginProgram {
 	Scanner sc = new Scanner(System.in);
-	MessageBoard[] board = new MessageBoard[1000];
-	static private String title, substance;
+	ArrayList<MessageBoard> st = new ArrayList<MessageBoard>();
+	private String title, substance;
 	int messageNum=0;
 	public MessageBoard(String newTitle, String newSub) {
 		this.title = newTitle;
@@ -37,9 +39,9 @@ public class MessageBoard extends LoginProgram {
 		case 2:
 			showBoard();
 			break;
-//		case 3:
-			
-//			break;
+		case 3:
+			delBoard();
+		break;
 		default:
             System.out.println("다시입력하세요.");
             break;
@@ -49,26 +51,44 @@ public class MessageBoard extends LoginProgram {
 	}
 }
 	public void writ() {
-		while(true) {
 			System.out.print("제목 \n>");
 			String newTitle = input();
 			
 			System.out.print("내용 \n>");
 			String newSub = input();
 			
-			board[messageNum++] = new MessageBoard(newTitle, newSub);
-			break;
-		}
+			st.add(messageNum++, new MessageBoard(newTitle, newSub));
+			
+			
+		
 	}
 	public void showBoard() {
-		for(int n=0; n<messageNum; n++) {
-			
-			System.out.println("번호 " + n + " 제목 : " + title);
+		for(int n=0; n<st.size(); n++) {			
+			System.out.println("번호 " + n + " 제목 : " + st.get(n).title);
 			}
-		System.out.print("선택 > ");
+		System.out.println("돌아가기 = 00");
+		System.out.print("선택 >");
 		int num = sc.nextInt();
-			System.out.println("제목 : " + title);
-			System.out.println("내용 : " + substance);
+		if(num != 00) {
+			System.out.println("제목 : " + st.get(num).title);
+			System.out.println("내용 : " + st.get(num).substance);
+		
+		}
+	}
+	
+	public void delBoard() {
+		System.out.print("삭제 번호 입력 \n>");
+		int m = sc.nextInt();
+		System.out.println("정말 삭제 하시겠습니까?");
+		System.out.println("1.삭제 2.돌아가기");
+		System.out.print(">");
+		int del = sc.nextInt();
+		if(del == 1) {
+		MessageBoard ab = st.remove(m); 
+		return;
+		}else if(del ==2) {
+		return ;
+		}
 	}
 	
 }
